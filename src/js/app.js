206c4161bf0a8f5d88ed6c5a268efe59f3233b0e
Betsija <43956352+Betsija@users.noreply.github.com>
2018-10-21 17:16:48 +0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+var showSecret = false;
+
+secretButton.addEventListener('click', toggleSecretState);
+updateSecretParagraph();
+
+function toggleSecretState() {
+    showSecret = !showSecret;
+    updateSecretParagraph();
+    updateSecretButton()
+}
+
+function updateSecretButton() {
+    if (showSecret) {
+        secretButton.textContent = 'Paslēpt';
+    } else {
+        secretButton.textContent = 'Parādīt';
+    }
+}
+
+function updateSecretParagraph() {
+    if (showSecret) {
+        secretParagraph.style.display = 'block';
+    } else {
+        secretParagraph.style.display = 'none';
+    }
+}
